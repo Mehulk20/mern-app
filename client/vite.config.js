5e4ce81151d4e86dab60c7977ce25688762e59b5
Mehul_Kumar <mehulkumarigiri@gmail.com>
2025-04-17 17:07:23 +0530

    change vite-config url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api/v1': {
-                target: 'http://127.0.0.1:5000',
+                target: JSON.stringify(import.meta.env.VITE_URL),
                 changeOrigin: true,
                 secure: false,
             },
