422b44f0ccdb32cc983d0f895d40b346e88224fd
Mehul_Kumar <mehulkumarigiri@gmail.com>
2025-04-17 17:19:16 +0530

    change vite-config url working-mode

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api/v1': {
-                target: JSON.stringify(import.meta.env.VITE_URL),
-                changeOrigin: true,
-                secure: false,
+    return {
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api/v1': {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                },
             },
         },
-    },
+    }
 })
